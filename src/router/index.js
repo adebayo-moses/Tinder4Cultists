87365c36bd95fb9882136b6710ec6783c5859377
Moses Adebayo <adebayomoses597@gmail.com>
2022-12-19 04:48:20 +0100

    build 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/About.vue')
+    },
+    // error 404
+    {
+      path: '/:catchAll(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFound.vue')
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="not-found">
+        <Header/>
+    <div class="container">
+            <div class="section-wrapper">
+    <div class="zero-item">
+                    <div class="zero-thumb">
+                        <img src="@/assets/404.png" alt="404" class="center">
+                    </div>
+                    <div class="zero-content">
+                        <h2>Oops! Love for where?</h2>
+                        <p>pretty sure your ribs is missing, can't be our page that's missing.  <i class="worried"></i></p>
+                        <RouterLink to="/" class="btn">Click here to go back to home page<i class="button-double-right"></i></RouterLink>
+                    </div>
+                </div>
+                </div>
+                </div>
+            </div>
+                <footer></footer>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+import header from '@/components/header.vue'
+import footer from '@/components/footer.vue'
+export default {
+    components
+    : {
+        header,
+        footer
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
